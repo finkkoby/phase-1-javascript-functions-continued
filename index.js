4ee1577a71ff414d15b6664f43876e851c8eaab2
Koby Fink <finkkoby@icloud.com>
2024-01-29 18:15:24 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(par = "roller-skate") {
+    return `This Saturday, I want to ${par}!`;
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(sym = "*") {
+    return function(adjective = "special") {
+        return `You are ${sym}${adjective}${sym}!`
+    }
+}
